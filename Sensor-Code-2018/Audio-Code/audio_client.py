20b3e7512ad379e48beaaf46fbfc757a27bca4b1
Max Godfrey <34620214+Max-Godfrey-2018@users.noreply.github.com>
2018-09-22 08:50:10 +0800

    Refactor audio_client.py

--- Sensor-Code-2018/Audio-Code/audio_client.py
+++ Sensor-Code-2018/Audio-Code/audio_client.py
@@ -1,32 +1,38 @@
+"""A pyaudio callback client"""
+
+import socket
+import time
 import constants
 import pyaudio
-from socket import socket
-from time import sleep
 
-audioClient = pyaudio.PyAudio()
-socketClient = socket()
-socketClient.connect((constants.HOST, constants.PORT))
-
-def callback(in_data, frame_count, time_info, status):
-    global socketClient
-    socketClient.send(in_data)
+def callback(client, in_data, frame_count, time_info, status):
+    """A callback function for the pyaudio.PyAudio audio stream"""
+    client.send(in_data)
     return in_data, pyaudio.paContinue
 
-stream = audioClient.open(
-    format=constants.WIDTH,
-    channels=constants.CHANNELS,
-    rate=constants.RATE,
-    input=True,
-    output=False,
-    stream_callback=callback
-)
+def main():
+    """audio_server.py: main function"""
+    audio = pyaudio.PyAudio()
+    socket_client = socket.socket()
+    socket_client.connect((constants.HOST, constants.PORT))
 
-stream.start_stream()
-while stream.is_active():
-    time.sleep(0.1)
+    stream = audio.open(
+        format=constants.WIDTH,
+        channels=constants.CHANNELS,
+        rate=constants.RATE,
+        input=True,
+        output=False,
+        stream_callback=callback
+    )
 
-stream.stop_stream()
-stream.close()
+    stream.start_stream()
+    while stream.is_active():
+        time.sleep(0.1)
 
-audioClient.terminate()
-socketClient.close()
+    stream.stop_stream()
+    stream.close()
+
+    audio.terminate()
+    socket_client.close()
+
+main()
